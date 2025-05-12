ead74a5eafd0c8ba8695b52de932c7cbf9da7d72
Jaehun Kim <jaehukim@c3r4s6.42gyeongsan.kr>
2025-05-12 20:02:42 +0900

    [Add] Create FileParser

--- includes/13_FileParser.hpp
+++ includes/13_FileParser.hpp
@@ -3,24 +3,34 @@
 
 class FileParser
 {
-private:
-    std::string _fileName;
-    FileParser();
+    private:
+        std::string     _fileName;
+        std::ifstream   _ifs;
+        std::streampos  _currentPos;
+        
+        FileParser();
+        std::string trimBuf(std::string &buf) const;
+        
+    public:
+        FileParser(char *fileName);
+        FileParser(const FileParser& rhs);
+        FileParser& operator=(const FileParser &rhs);
+        ~FileParser();
 
-public:
-    FileParser(char *fileName);
-    FileParser(const FileParser& rhs);
-    FileParser& operator=(const FileParser &rhs);
-    ~FileParser();
+        void        setFileName(const std::string fileName);
+        void        setIfstream(const std::string fileName);
+        void        setPosition(std::streampos pos);
+        std::string     getFileName() const;
+        std::ifstream&  getIfstream() const;
+        std::streampos  getPosition() const;
 
-    void        setFileName(const std::string fileName);
-    std::string getFileName() const;
+        void    Parse() const;
+        
 
-    void    Parse() const;
-    
-    class FileNotOpenedException: public std::exception
-    {
-        public:
-            const char* what() const throw();
-    };
+
+        class FileNotOpenedException: public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
 };
--- srcs/13_FileParser.cpp
+++ srcs/13_FileParser.cpp
@@ -7,7 +7,11 @@ FileParser::FileParser()
 
 FileParser::FileParser(char *fileName)
 {
+    std::ifstream   ifs;
+
     this->_fileName = fileName;
+    ifs.open(this->_fileName.c_str());
+    setIfstream(ifs);
     Parse();
 }
 
@@ -36,17 +40,50 @@ void    FileParser::setFileName(const std::string fileName)
     this->_fileName = fileName;
 }
 
+void    FileParser::setIfstream(std::ifstream ifs)
+{
+    if (!ifs.is_open())
+    {
+        throw FileNotOpenedException();
+    }
+    this->_ifs;
+}
+
+void    FileParser::setPosition(std::streampos pos)
+{
+    this->_currentPos = pos;
+}
+
 std::string FileParser::getFileName() const
 {
     return (this->_fileName);
 }
 
+std::string FileParser::trimBuf(std::string &buf) const
+{
+    size_t      annotPos;
+    size_t      start;
+    size_t      end;
+    std::string output;
+    
+    annotPos = buf.find("#");
+    if (annotPos != std::string::npos)
+        output = buf.substr(0, annotPos);
+    else
+        output = buf;
+    
+    start = output.find_first_not_of(" \t");
+    if (start == std::string::npos)
+        start = 0;
+    end = output.find_last_not_of("#");
+
+    output = output.substr(start, end - start - 1);
+    return (output);
+}
+
 void    FileParser::Parse()  const
 {
-    std::ifstream   ifs;
     std::string     buf;
-
-    ifs.open(this->_fileName.c_str());
     
     if (ifs.is_open() == false)
         throw FileNotOpenedException();
@@ -54,9 +91,8 @@ void    FileParser::Parse()  const
     buf.clear();
     while (getline(ifs, buf))
     {
-        buf.find_last_not_of('#');
-        buf.find_first_not_of('\t');
-        std::cout << buf << std::endl;
+        std::cout << trimBuf(buf) << std::endl;
+        buf.clear();
     }
 }
 
