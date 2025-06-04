d3848c44034108cb014493f18dddd3824574944c
Kangwoo Jung <kjung@c3r3s5.42gyeongsan.kr>
2025-06-04 18:49:10 +0900

    push

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,11 @@
 // 버튼 클릭 시 실행될 함수
 function showAlert() {
-    alert("버튼을 클릭했습니다!");
+    window.location.href = '/webserv_tester.html';
 }
 
 // 모든 버튼에 클릭 이벤트 추가
 const elements = document.getElementsByClassName("button");
+
 
 for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", showAlert);
